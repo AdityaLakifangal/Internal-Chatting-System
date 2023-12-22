bf077a8c2765b8b2797fa68f2691095bc40368a7
AdityaLakifangal <aadityalakifangal@gmail.com>
2023-12-22 10:28:13 +0530

    Uploaded MainFrame1

---
+++ Source_Code/MainFrame_1.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame_1 extends JFrame {
+
+    public void initialize(User user) {
+        JFrame frame = new JFrame("Chat Application");
+
+        JLabel label = new JLabel("Welcome to the chat application");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        
+        JButton okbutton = new JButton("OK");
+        
+        okbutton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                frame.dispose();
+            }
+        });
+        
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(label, BorderLayout.CENTER);
+        panel.add(okbutton, BorderLayout.SOUTH);
+        panel.setBackground(new Color(102, 255, 102));
+
+        frame.add(panel);
+        
+        frame.setBounds(200, 325, 400, 200);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
